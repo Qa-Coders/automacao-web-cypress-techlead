1ffcecb884594485d525e971bf9a1d0c74f5412c
wasilva <contato.wasilva@hotmail.com>
2024-01-24 21:05:40 -0300

    fist commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+Cypress.Commands.add('Login', (email, password) => {
+    cy.get('#email').type(email);
+    cy.get('#password').type(password);
+    cy.get('#login').click();
+    
+    cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('/login')
+    })
+
+})
+
+Cypress.Commands.add('MsgLoginFalho', () => {
+    cy.get('[class="MuiAlert-message css-1xsto0d"]').should('be.visible');
+    cy.get('[class="MuiAlert-message css-1xsto0d"]').should('have.text','E-mail e/ou senha inválidos');
+});
